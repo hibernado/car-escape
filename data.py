f6bebeb932fe3073103b1c7508ac9ce347c8b2e6
hibernado <oboyle.a@gmail.com>
2020-04-11 08:29:56 +0100

    add levels

--- data.py
+++ data.py
@@ -21,6 +21,32 @@ vehicle_sets = [
      Car(4, 2, 'vertical', BLUE),
      Car(2, 0, 'vertical', PINK),
      Lorry(3, 5, 'horizontal', YELLOW),
-     Car(3, 0, 'horizontal', DARKGREY)
-     ]
+     Car(3, 0, 'horizontal', DARKGREY)],
+
+    [Car(1, 3, 'horizontal', RED),
+     Lorry(3, 1, 'vertical', YELLOW),
+     Car(1, 0, 'vertical', ORANGE),
+     Car(1, 2, 'horizontal', GREEN),
+     Lorry(5, 0, 'vertical', LIGHTPURPLE),
+     Car(2, 0, 'horizontal', BLUE)],
+
+    [Car(1, 3, 'horizontal', RED),
+     Lorry(3, 3, 'vertical', LIGHTPURPLE),
+     Lorry(0, 3, 'vertical', YELLOW),
+     Car(2, 1, 'vertical', GREEN),
+     Lorry(3, 2, 'horizontal', DARKBLUE),
+     Lorry(2, 0, 'horizontal', DARKGREEN),
+     Car(5, 0, 'vertical', ORANGE)],
+
+    [Car(1, 3, 'horizontal', RED),
+     Lorry(0, 2, 'vertical', LIGHTPURPLE),
+     Car(0, 5, 'horizontal', GREEN),
+     Lorry(3, 3, 'vertical', YELLOW),
+     Car(0, 0, 'vertical', PINK),
+     Lorry(1, 2, 'horizontal', DARKGREEN),
+     Lorry(4, 2, 'vertical', BLUE),
+     Car(5, 2, 'vertical', DARKGREY),
+     Car(5, 4, 'vertical', ORANGE),
+     Car(4, 1, 'horizontal', PURPLE),
+     Car(4, 0, 'horizontal', MEDIUMGREEN)]
 ]
