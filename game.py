a6b6ac6699eb481694b9060ea204fc70ffe802bf
hibernado <oboyle.a@gmail.com>
2020-04-12 15:23:28 +0100

    cleaner vehicle move logic

--- game.py
+++ game.py
@@ -1,21 +1,22 @@
-from euclid.dim2 import Path
-from geom import Square, Position, Space
-from vehicles import Vehicle
-from constants import VEHICLE_SETS, BOARD_SIZE
 from board import Board
+from constants import VEHICLE_SETS, BOARD_SIZE
+from euclid.dim2 import Path
+from geom import Position
+from vehicles import Vehicle
 from vehicles import get_vehicle_from_config
 
 
-def vehicle_on_board(board: Board, vehicle: Vehicle, position):
+def vehicle_off_board(board: Board, vehicle: Vehicle, position):
     # if vehicle.colour == self.red and position.x > max(self.spaces)[0]:
     #     return True
-    vob = vehicle.get_positions(position).intersection(board.positions) == vehicle.get_positions(position)
-    if not vob:
+    vehicle_on_board = vehicle.get_positions(position).intersection(board.positions) == vehicle.get_positions(position)
+    if not vehicle_on_board:
         print('CANNOT MOVE VEHICLE OFF BOARD')
-    return vob
+        return True
 
 
-def path_is_free(vehicle: Vehicle, vehicles: list, new_position: Position):
+
+def path_contains_obstacle(vehicle: Vehicle, vehicles: list, new_position: Position):
     print("{}:{}".format(vehicle.position, new_position))
     positions_in_path = set()
     for p in Path(vehicle.position, new_position).get_vectors():
@@ -28,19 +29,20 @@ def path_is_free(vehicle: Vehicle, vehicles: list, new_position: Position):
 
     if positions_filled_by_other_cars.intersection(positions_in_path):
         print('PATH IS NOT FREE')
-        return False
-    return True
+        return True
 
 
-def valid_move(vehicle: Vehicle, new_position: Position):
+def invalid_move(vehicle: Vehicle, new_position: Position):
     valid = vehicle.valid_move(new_position)
     if not valid:
         print('NOT A VALID VEHICLE MOVE !!!')
-    return valid
+        return True
 
 
-def vehicle_moving(vehicle: Vehicle, new_position: Position):
-    return vehicle.position != new_position
+def vehicle_not_moving(vehicle: Vehicle, new_position: Position):
+    if vehicle.position == new_position:
+        return True
+
 
 
 class Game:
@@ -69,12 +71,9 @@ class Game:
         self.move_vehicle(self.selected_vehicle, new_position)
 
     def move_vehicle(self, vehicle: Vehicle, new_position: Position):
-        if not vehicle_moving(vehicle, new_position):
-            return None
-        if not valid_move(vehicle, new_position):
-            return None
-        if not vehicle_on_board(self.board, vehicle, new_position):
-            return None
-        if not path_is_free(vehicle, self.vehicles, new_position):
+        if vehicle_not_moving(vehicle, new_position) or\
+                invalid_move(vehicle, new_position) or\
+                vehicle_off_board(self.board, vehicle, new_position) or\
+                path_contains_obstacle(vehicle, self.vehicles, new_position):
             return None
         vehicle.move_to(new_position)
